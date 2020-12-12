3fd0e099913fa299f8a7f347dba7660056978a97
unknown <kalanags331@gmail.com>
2020-12-12 17:21:40 +0530

    Develop Basic App for Assignment

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask
+from flask import Flask, request, render_template, url_for
+from jinja2 import Environment
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/submit')
+def submit():
+    username = request.values.get('usernameText')
+    password = request.values.get('passwordText')
+
+    if password == 'sliit123':
+        return render_template('home.html', greeting=" Welcome %s"%(username))
+    else: 
+        return render_template('index.html', message="Invalid credentials!")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
